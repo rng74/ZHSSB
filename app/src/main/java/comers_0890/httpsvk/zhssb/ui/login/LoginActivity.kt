972b62573d487b4d88c64e16e161419367d6ab2f
rng74 <nadyrcoop@gmail.com>
2019-12-01 02:16:24 +0600

    Added layout

---
+++ app/src/main/java/comers_0890/httpsvk/zhssb/ui/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package comers_0890.httpsvk.zhssb.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import comers_0890.httpsvk.zhssb.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
