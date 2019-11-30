53b6a4c5baaf810cf8b49a3468bba5e5bc08d10e
Zhassulan Urazbayev <zhassulan.urazbaev@gmail.com>
2019-12-01 00:50:51 +0600

    Added registration activity

---
+++ app/src/main/java/comers_0890/httpsvk/zhssb/ui/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package comers_0890.httpsvk.zhssb.ui.registration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import comers_0890.httpsvk.zhssb.R
+
+class RegistrationActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration_layout)
+    }
+}
