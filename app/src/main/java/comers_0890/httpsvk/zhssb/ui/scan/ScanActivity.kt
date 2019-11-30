25e6a4242ad38835bcaa6c778485771af6fbfcf3
rng74 <nadyrcoop@gmail.com>
2019-12-01 00:02:42 +0600

    Added ScanActivity

---
+++ app/src/main/java/comers_0890/httpsvk/zhssb/ui/scan/ScanActivity.kt
@@ -0,0 +1,13 @@
+package comers_0890.httpsvk.zhssb.ui.scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import comers_0890.httpsvk.zhssb.R
+
+class ScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
